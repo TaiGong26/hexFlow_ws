f811f25f3b0acb3b82d044854b1acac0570a1640
TheTaiGon <thetaigon@qq.com>
2026-05-11 17:44:24 +0800

    change robot test launch

--- src/hex_flow_ros_robot/launch/archer_y6_test.launch.py
+++ src/hex_flow_ros_robot/launch/archer_y6_test.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     
